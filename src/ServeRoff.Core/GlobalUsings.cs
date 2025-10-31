96409ad4e854e5b4fc244328ce02cb7c15a421f1
DeganItsme <kj47xo@gmail.com>
2025-10-31 10:51:18 +0300

    Create GlobalUsings.cs

---
+++ src/ServeRoff.Core/GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using ServeRoff.Core.Entities;
+global using ServeRoff.Core.Enums;
+global using ServeRoff.Core.Interfaces;
+global using ServeRoff.Core.Records;
+global using ServeRoff.Core.Requests;
+global using ServeRoff.Core.Responses;
