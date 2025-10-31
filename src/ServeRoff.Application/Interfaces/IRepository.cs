8251a7f56a4faced1ff0bfe7cd60f4622ff07dae
DeganItsme <kj47xo@gmail.com>
2025-10-31 10:52:42 +0300

    Create IRepository.cs

---
+++ src/ServeRoff.Application/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+namespace ServeRoff.Application.Interfaces;
+
+public interface IRepository<TEntity> where TEntity : class, IEntity
+{
+    Task AddAsync(TEntity entity, CancellationToken cancellationToken = default);
+    Task UpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
+    Task DeleteAsync(TEntity entity, CancellationToken cancellationToken = default);
+    
+    Task<TEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task SaveChangesAsync(CancellationToken cancellationToken = default);
+}
